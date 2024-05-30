edbf4896ddcf5f7fb8f29a24fc8b55378aeac3c5
sunaggn <sunagegin@gmail.com>
2024-05-30 03:29:13 +0300

    gift

--- Assets/Scripts/Buttons/GiftAchimentButton.cs
+++ Assets/Scripts/Buttons/GiftAchimentButton.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class GiftAchimentButton : MonoBehaviour
 {
 
-    [SerializeField] private int Index;
+   
+    [SerializeField] private string Index;
     [SerializeField] private Questions questions;
-
     [SerializeField] private GameObject panel;
     public void OpenGift()
     {
-        questions.Achievements_Index = Index;
+        Calisartik(); 
         panel.SetActive(true);
     }
     public void CloseGift()
@@ -19,5 +19,16 @@ public class GiftAchimentButton : MonoBehaviour
         panel?.SetActive(false);
     }
 
-    
+    public void Calisartik()
+    {
+        if (Index == "a") questions.Achievements_Index = 0;
+        else if (Index == "b") questions.Achievements_Index = 1;
+        else if (Index == "c") questions.Achievements_Index = 2;
+        else if (Index == "d") questions.Achievements_Index = 3;
+        else if (Index == "e") questions.Achievements_Index = 4;
+        else if (Index == "f") questions.Achievements_Index = 5;
+        else if (Index == "g") questions.Achievements_Index = 6;
+        else if (Index == "h") questions.Achievements_Index = 7;
+        else if (Index == "i") questions.Achievements_Index = 8;
+    }
 }
--- Assets/Scripts/Datas/Questions.cs
+++ Assets/Scripts/Datas/Questions.cs
@@ -9,6 +9,8 @@ public class Questions : ScriptableObject
     [Header("Config")]
     [Header("Questions")]
 
+    [SerializeField] public int FunFactindex, funfactstart, funfactfinal;
+    [SerializeField] public string[] FunFacts;
     [SerializeField] public int Achievements_Index;
     [SerializeField] public string[] Achievements_String;
     [SerializeField] public int SelectedCategorie;
